cdda80c19fc829a9d3c4ea141424796371278054
Aizaz Ul Haq <aizaz.ulhaq@msn.com>
2024-11-06 23:30:41 +0500

    updated deploy script to force rebuild of images

--- infrastructure/scripts/deploy-local.sh
+++ infrastructure/scripts/deploy-local.sh
@@ -3,5 +3,5 @@
 # Navigate to the directory containing the Docker Compose file
 cd "$GITHUB_ROOT/instawork-take-home/infrastructure/docker/compose" || exit
 
-# Run Docker Compose
-docker-compose -f local.yaml up -d
+# Run Docker Compose with force rebuild
+docker-compose -f local.yaml up -d --build
