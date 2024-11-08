1f5b022dcf29b9e3b00ef75eb292bc21e0dae95e
Aizaz Ul Haq <aizaz.ulhaq@msn.com>
2024-11-08 17:12:12 +0500

    changed deploy script. Added wipe option to remove containers and delete local data

--- infrastructure/scripts/deploy-local.sh
+++ infrastructure/scripts/deploy-local.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Navigate to the directory containing the Docker Compose file
 cd "$GITHUB_ROOT/instawork-take-home/infrastructure/docker/compose" || exit
 
-# Run Docker Compose with force rebuild
+read -p "Do you want to wipe data? (y/n): " confirm
+if [[ "$confirm" == "y" ]]; then
+    docker-compose -f local.yaml down
+    rm -rf "$GITHUB_ROOT/instawork-take-home/data" || exit
+else
+    echo "Data not wiped....."
+fi
+
 docker-compose -f local.yaml up -d --build
